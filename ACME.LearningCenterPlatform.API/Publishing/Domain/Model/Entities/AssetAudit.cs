bdfd70dfdd868e480a9f335d69a3547b548e6bf0
Angel Velasquez <pcisavel@upc.edu.pe>
2024-10-07 13:49:53 -0500

    fix: rename asset partial class for audit aspect.

--- ACME.LearningCenterPlatform.API/Publishing/Domain/Model/Entities/AssetAudit.cs
+++ ACME.LearningCenterPlatform.API/Publishing/Domain/Model/Entities/AssetAudit.cs
@@ -3,7 +3,7 @@ using EntityFrameworkCore.CreatedUpdatedDate.Contracts;
 
 namespace ACME.LearningCenterPlatform.API.Publishing.Domain.Model.Entities;
 
-public partial class AssetAudit: IEntityWithCreatedUpdatedDate
+public partial class Asset: IEntityWithCreatedUpdatedDate
 {
     [Column("CreatedAt")] public DateTimeOffset? CreatedDate { get; set; }
     [Column("UpdatedAt")] public DateTimeOffset? UpdatedDate { get; set; }
