c7280ea2a0eba335ad56381901b174f19b45d666
Angel Velasquez <pcisavel@upc.edu.pe>
2024-11-05 20:29:26 -0500

    feat(iam): implement token service.

--- ACME.LearningCenterPlatform.API/IAM/Infrastructure/Tokens/JWT/Services/TokenService.cs
+++ ACME.LearningCenterPlatform.API/IAM/Infrastructure/Tokens/JWT/Services/TokenService.cs
@@ -32,8 +32,29 @@ public class TokenService(IOptions<TokenSettings> tokenSettings) : ITokenService
         return token;
     }
 
-    public Task<int?> ValidateToken(string token)
+    public async Task<int?> ValidateToken(string token)
     {
-        throw new NotImplementedException();
+        if (string.IsNullOrEmpty(token)) return null;
+        var tokenHandler = new JsonWebTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_tokenSettings.Secret);
+        try
+        {
+            var tokenValidationResult = await tokenHandler.ValidateTokenAsync(token, new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ClockSkew = TimeSpan.Zero
+            });
+            var jwtToken = (JsonWebToken)tokenValidationResult.SecurityToken;
+            var userId = int.Parse(jwtToken.Claims.First(claim => claim.Type == ClaimTypes.Sid).Value);
+            return userId;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return null;
+        }
     }
 }
